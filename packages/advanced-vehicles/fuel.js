a3ecea423831cbfe2bf1b29ef37e609893102407
MrPancakers <br3ttscott@gmail.com>
2020-04-05 21:07:30 +1000

    Fixed warnings going off if no data changed

--- packages/advanced-vehicles/fuel.js
+++ packages/advanced-vehicles/fuel.js
@@ -1,4 +1,4 @@
 mp.events.add('saveFuel', (player, data) => {
     let oldFuel = player.vehicle.getVariable('fuel');
-    oldFuel > data ? player.vehicle.setVariable('fuel', data) : console.log(`${player.name} potentially cheated fuel (Old: ${oldFuel} / New: ${data})`);
+    oldFuel >= data ? player.vehicle.setVariable('fuel', data) : console.log(`${player.name} potentially cheated fuel (Old: ${oldFuel} / New: ${data})`);
 });
